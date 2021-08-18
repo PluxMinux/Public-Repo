f80f3925d0c3b32660900c1f913928a5adbbeb93
PnM <64911289+PluxMinux@users.noreply.github.com>
2021-08-18 02:18:20 -0800

    Update Working_with_email.py

--- Working_with_email.py
+++ Working_with_email.py
@@ -1,15 +1,25 @@
 import os
 import smtplib
-
+email = os.environ.get('USER_EMAIL')
+passw = os.environ.get('USER_PASS')
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+#with smtplib.SMTP('localhost',1025) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
-
-    smtp.login(EMAIL, PASSWORD)
+    smtp.login(email, passw)
+ 
 
     subject = 'Python testing email.'
     body = 'testing 001'
     msg = f'Subject: {subject}\n\n{body}'
     
-    smtp.sendmail(SENDER, RECEIVER ,msg)
+    smtp.sendmail(email, 'aaaaalven@gmail.com' ,msg)
+
+# server = smtplib.SMTP('smtp.gmail.com',587)
+# server.starttls()
+# server.login(email, passw)
+# subject = 'Python testing email.'
+# body = 'testing 001'
+# msg = f'Subject: {subject}\n\n{body}'
+# server.sendmail('alven.toraja@gmail.com', 'aaaaalven@gmail.com' ,msg)
